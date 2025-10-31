84ff180a7f99e3a4e88f1158bd97954aa93c1ef0
IsaacUnU <isaaclopeztur777@gmail.com>
2025-10-31 18:36:46 +0100

    Segundo commit

--- imprimirNombre.java
+++ imprimirNombre.java
@@ -2,8 +2,10 @@ public class imprimirNombre {
     public static void main(String[] args) {
     
         for(int i = 0; i < 50; i++) {
-            System.out.println("Isaac");
+            System.out.println("Isaac que tal estas???");
+            System.out.println("-------------------------");
         }
 
+        System.out.println("Fin del programa");
     }
 }
